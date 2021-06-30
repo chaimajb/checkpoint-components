6412948f7e9990d96e3b06c2e8ea2b811e83181a
chaimajb <jabrichaima1@gmail.com>
2021-06-30 21:48:31 +0100

    second commit it

--- src/Component/Profile/ProfilePhoto.js
+++ src/Component/Profile/ProfilePhoto.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Profile_photo () {
-    return <img src="/dualipa.jpg" width='200'  ></img>
+    return <img src="/dualipa.jpg" width='200'  />
 }
 
 export default Profile_photo;
